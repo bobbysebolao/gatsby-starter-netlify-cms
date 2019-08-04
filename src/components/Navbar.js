5f108aaef5702897064cc5c70347012c6f173246
Bobby <bobbysebolao@gmail.com>
2019-08-04 18:09:59 +0100

    Rename navbar top level anchor tag text

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+// import github from '../img/github-icon.svg'
+import logo from '../img/logo2.svg'
 
 const Navbar = class extends React.Component {
   constructor(props) {
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <img src={logo} alt="Tiny House Advice" />
             </Link>
             {/* Hamburger menu */}
             <div
@@ -61,32 +61,22 @@ const Navbar = class extends React.Component {
           >
             <div className="navbar-start has-text-centered">
               <Link className="navbar-item" to="/about">
-                About
+                Tiny House Living
               </Link>
               <Link className="navbar-item" to="/products">
-                Products
+                Tiny House Designs
               </Link>
               <Link className="navbar-item" to="/blog">
-                Blog
+                DIY Tiny House Kits
               </Link>
               <Link className="navbar-item" to="/contact">
-                Contact
+                Tiny House Cost Breakdown
               </Link>
               <Link className="navbar-item" to="/contact/examples">
-                Form Examples
+                Tiny House Laws by State
               </Link>
             </div>
             <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
             </div>
           </div>
         </div>
